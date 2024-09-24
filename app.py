b0815643354a0fc4d2a8cf8de9702d8d6842b379
SantosASM <andrey-santos@hotmail.com>
2024-09-24 14:54:18 -0300

    Update COL RENAME CONDI app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ port = int(os.environ.get("PORT", 8501))
 # Carregar os dados
 car_data = pd.read_csv("vehicles.csv", sep=",")
 
-car_data = car_data.rename(columns={'model_year': 'year'})
+car_data = car_data.rename(columns={'model_year': 'year', 'condition: 'condition'})
 
 # Caixa de seleção para filtrar por condição do veículo
 condition_options = car_data['condition'].unique()
