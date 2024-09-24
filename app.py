6db1aa5e7a1e0a5b86655cfee9d5d84c05abae5c
SantosASM <andrey-santos@hotmail.com>
2024-09-24 16:07:40 -0300

    Update SIMPLIFICADO YEAR  app.py

--- app.py
+++ app.py
@@ -8,29 +8,27 @@ port = int(os.environ.get("PORT", 8501))
 
 # Carregar os dados
 car_data = pd.read_csv("vehicles.csv", sep=",")
-    # Botão para criar o histograma
-    hist_button = st.button('Criar histograma')
 
-    if hist_button:
-        st.header("Histograma dos dados")
-        st.write(f'Criando um histograma para veículos do ano: {selected_year}')
-        
-        # Criar um histograma
-        fig_hist = px.histogram(filtered_data, x="odometer", title=f"Histograma: Hodômetro dos veículos do ano {selected_year}")
-        
-        # Exibir o gráfico de histograma
-        st.plotly_chart(fig_hist, use_container_width=True)
+# Botão para criar o histograma
+hist_button = st.button('Criar histograma')
 
-    # Botão para criar o gráfico de dispersão
-    scatter_button = st.button('Criar gráfico de dispersão')
+if hist_button:
+    st.header("Histograma dos dados")
+    
+    # Criar um histograma
+    fig_hist = px.histogram(car_data, x="odometer", title="Histograma: Hodômetro dos veículos")
+    
+    # Exibir o gráfico de histograma
+    st.plotly_chart(fig_hist, use_container_width=True)
 
-    if scatter_button:
-        st.header("Gráfico de Dispersão")
-        st.write(f'Criando um gráfico de dispersão entre a idade do veículo e o preço para veículos do ano: {selected_year}')
-        
-        # Criar um gráfico de dispersão
-        fig_scatter = px.scatter(filtered_data, x="year", y="price", color="year", title=f"Dispersão: Ano do Veículo vs Preço ({selected_year})")
-        
-        # Exibir o gráfico de dispersão
-        st.plotly_chart(fig_scatter, use_container_width=True)
+# Botão para criar o gráfico de dispersão
+scatter_button = st.button('Criar gráfico de dispersão')
 
+if scatter_button:
+    st.header("Gráfico de Dispersão")
+    
+    # Criar um gráfico de dispersão
+    fig_scatter = px.scatter(car_data, x="year", y="price", color="year", title="Dispersão: Ano do Veículo vs Preço")
+    
+    # Exibir o gráfico de dispersão
+    st.plotly_chart(fig_scatter, use_container_width=True)
