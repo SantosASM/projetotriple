a04484b8af7c576afc18f75c77f4c9281edb820a
SantosASM <andrey-santos@hotmail.com>
2024-09-24 17:27:20 -0300

    Update FINAL app.py

--- app.py
+++ app.py
@@ -7,25 +7,17 @@ import plotly.express as px
 port = int(os.environ.get("PORT", 8501))
 
 # Carregar os dados
-car_data = pd.read_csv("vehicles.csv", sep=",")
+car_data = pd.read_csv(r"C:\Users\andre\OneDrive\Documentos\vehicles.csv")
 
-# Função para renomear todas as colunas
-def rename_columns(df):
-    df.columns = df.columns.str.lower().str.replace(' ', '_').str.replace(':', '').str.replace('-', '_')
-    return df
+# Verifique se a coluna 'model_year' existe
+if 'model_year' not in car_data.columns:
+    st.error("A coluna 'model_year' não foi encontrada no arquivo CSV. Por favor, verifique o arquivo de dados.")
+else:
+    selected_year = st.selectbox("Selecione o ano do veículo", options=car_data['model_year'].unique())
 
-# Renomear todas as colunas
-car_data = rename_columns(car_data)
+    # Filtrar os dados com base no ano selecionado
+    filtered_data = car_data[car_data['model_year'] == selected_year]
 
-# Verificar se a coluna 'year' (ano) existe no DataFrame
-if 'year' in car_data.columns:
-    # Caixa de seleção para filtrar por ano do veículo
-    year_options = car_data['year'].unique()
-    selected_year = st.selectbox("Selecione o ano do veículo", options=year_options)
-
-    # Filtrar os dados pelo ano selecionado
-    filtered_data = car_data[car_data['year'] == selected_year]
-    
     # Botão para criar o histograma
     hist_button = st.button('Criar histograma')
 
@@ -47,9 +39,7 @@ if 'year' in car_data.columns:
         st.write(f'Criando um gráfico de dispersão entre a idade do veículo e o preço para veículos do ano: {selected_year}')
         
         # Criar um gráfico de dispersão
-        fig_scatter = px.scatter(filtered_data, x="year", y="price", color="year", title=f"Dispersão: Ano do Veículo vs Preço ({selected_year})")
+        fig_scatter = px.scatter(filtered_data, x="model_year", y="price", color="skyblue", title=f"Dispersão: Ano do Veículo vs Preço ({selected_year})")
         
         # Exibir o gráfico de dispersão
         st.plotly_chart(fig_scatter, use_container_width=True)
-else:
-    st.error("A coluna 'year' não foi encontrada no arquivo CSV. Por favor, verifique o arquivo de dados.")
